63b7421b0eecbc073d5e664b3c9aeb43cc2f4208
Artem <118046594+Artem160188@users.noreply.github.com>
2022-12-19 21:21:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,40 @@
-﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-Console.WriteLine($"\nВведите размер массива m x n :");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.WriteLine($"\nВведите размер массива m x n:");
 int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
 int[,] array = new int[m, n];
 CreateArray(array);
 WriteArray(array);
 
-Console.WriteLine($"\nОтсортированный массив: ");
-OrderArrayLines(array);
-WriteArray(array);
-
-void OrderArrayLines(int[,] array)
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(1) - 1; k++)
-            {
-                if (array[i, k] < array[i, k + 1])
-                {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
-                }
-            }
-        }
+        sumLine = tempSumLine;
+        minSumLine = i;
     }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
 }
 
 int InputNumbers(string input)
